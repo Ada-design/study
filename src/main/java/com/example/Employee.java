0868c3a0dde38c7576f07df59bf529120fcdfe60
baixue <1683700219@qq.com>
2026-07-14 18:00:50 +0800

    修改后的员工信息

--- src/main/java/com/example/Employee.java
+++ src/main/java/com/example/Employee.java
@@ -26,35 +26,4 @@ public class Employee {
     private Country country;
 
     private LocalDate hireDay;
-
-    private Address address;
-}
-package com.example;
-
-import java.math.BigDecimal;
-import java.time.LocalDate;
-
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.Setter;
-
-/**
- * @description: 员工
- * 
- * @author 16837
- * @date 2026-07-14 15:23:55
- */
-
-@AllArgsConstructor
-@Getter
-@Setter
-public class Employee {
-
-    private String name;
-
-    private BigDecimal salary;
-
-    private Country country;
-
-    private LocalDate hireDay;
 }
